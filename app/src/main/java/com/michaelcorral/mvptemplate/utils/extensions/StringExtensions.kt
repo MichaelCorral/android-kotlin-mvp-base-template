279c2797b62e22c8543ecdf599ad393bb87cb714
Michael Corral <mcorral@stratpoint.com>
2020-06-02 19:00:06 +0800

    Added frequently used extensions

---
+++ app/src/main/java/com/michaelcorral/mvptemplate/utils/extensions/StringExtensions.kt
@@ -0,0 +1,16 @@
+package com.michaelcorral.mvptemplate.utils.extensions
+
+fun String.isPhone(): Boolean {
+    val p = "^1([34578])\\d{9}\$".toRegex()
+    return matches(p)
+}
+
+fun String.isEmail(): Boolean {
+    val p = "^(\\w)+(\\.\\w+)*@(\\w)+((\\.\\w+)+)\$".toRegex()
+    return matches(p)
+}
+
+fun String.isNumeric(): Boolean {
+    val p = "^[0-9]+$".toRegex()
+    return matches(p)
+}
